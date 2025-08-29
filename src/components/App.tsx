c3a58924ba12bf98a16892d8a047d40820cd7499
Vlad-dualv <147710428+Vlad-dualv@users.noreply.github.com>
2025-08-29 18:22:57 +0100

    exercise 2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,28 @@
-interface User {
-  id: number;
-  name: string;
-  email:string;
-  isActive: boolean;
-  age?: number;
+interface CreditCard {
+  type: "creditCard";
+  number: string;
 }
 
-function createUser(user: User): User {
-  return {...user};
+interface Paypal {
+  type: "paypal";
+  email: string;
 }
 
-const user = createUser({
-  id: 1,
-  name: 'John Doe',
-  email: "john@example.com",
-  isActive: true,
-  age: 30,
-});
-console.log(user);
+type PaymentMethod = CreditCard | Paypal;
+
+function processPayment(method: PaymentMethod): string {
+  if (method.type === "creditCard") {
+    return "Payment by credit card"
+  }
+  if (method.type === "paypal") {
+    return "Payment method by paypal"
+  }
+  return "Unknown payment method"
+}
+
+console.log(processPayment({type: "creditCard", number: "4532-1234-5678-9012"})); // Credit card
+console.log(processPayment({type: "paypal", email: "4532-1234-5678-9012"})); // Credit card
+
 
 export default function App() {
   return <div>Check the console for output</div>;
