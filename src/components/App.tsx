3081bcf951bfb11219cf1b1c94d5e81e854f85f1
Vlad-dualv <147710428+Vlad-dualv@users.noreply.github.com>
2025-08-29 19:06:20 +0100

    exercise 4

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,68 +1,17 @@
-interface Product {
-  id: number;
-  name: string;
-  price: number;
-  inStock: boolean;
-}
+type Id = string | number;
 
-const products: Product[] = [
-    {
-    id: 1,
-    name: "MacBook Pro 14-inch",
-    price: 1999.99,
-    inStock: true
-  },
-  {
-    id: 2,
-    name: "iPhone 15",
-    price: 799.99,
-    inStock: false
-  },
-  {
-    id: 3,
-    name: "AirPods Pro",
-    price: 249.99,
-    inStock: true
-  },
-  {
-    id: 4,
-    name: "iPad Air",
-    price: 599.99,
-    inStock: true
-  },
-  {
-    id: 5,
-    name: "Apple Watch Series 9",
-    price: 399.99,
-    inStock: false
-  },
-  {
-    id: 6,
-    name: "Magic Mouse",
-    price: 79.99,
-    inStock: true
-  },
-  {
-    id: 7,
-    name: "Studio Display",
-    price: 1599.99,
-    inStock: false
-  },
-  {
-    id: 8,
-    name: "Magic Keyboard",
-    price: 179.99,
-    inStock: true
+function convert(id: Id): string | number {
+  if (typeof(id) === "string") {
+    return id.toUpperCase()
+  } else if (typeof(id) === "number") {
+    return id * 10
+  } else {
+    return "Unknown type id"
   }
-]
-
-function getInStockProducts(products: Product[]): Product[] {
-  return products.filter(product => product.inStock)
 }
 
-const inStockProducts = getInStockProducts(products);
-
-console.log(inStockProducts)
+console.log(convert(10))
+console.log(convert("fadfdsf262"))
 
 export default function App() {
   return <div>Check the console</div>
