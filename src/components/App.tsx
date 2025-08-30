7d7a0262ba71f40e33d2ff6b8d6604189f1c661e
Vlad-dualv <147710428+Vlad-dualv@users.noreply.github.com>
2025-08-30 21:22:50 +0100

    exercise1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,32 +1,27 @@
-class BankAccount {
-  constructor(private balance: number, public accountNumber: number, protected customerName: string) {}
-  deposit(value: number): number | string {
-    if (value > 0) {
-      this.balance += value
-      return this.balance
-    }
-    return "Deposit must be more than 0"
-  }
-  withdraw(value: number): number | string {
-    if (value <= 0) {
-      return "Withdraw must be more than 0";
-    }
-    if (value > this.balance) {
-      return "Insufficient balance";
-    }
-    this.balance -= value;
-    return this.balance;
-  }
-  getBalance(): number {
-    return this.balance
-  }
- }
+type Role = "guest" | "admin"
 
- const account = new BankAccount(1000, 12345, "John")
- console.log(account.accountNumber); // 12345 (accessible - public)
-console.log(account.deposit(500)); // 1500
-console.log(account.withdraw(200)); // 1300
-console.log(account.getBalance()); // 1300
+interface User {
+  name: string;
+  age?: number;
+  email: string;
+  role: Role;
+}
+
+function checkAdmin(user: User): boolean {
+  if (user.role === "admin") {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(checkAdmin({
+  name: "John",
+  age: 30,
+  email: "example@example.com",
+  role: "guest"
+}))
+
 
 export default function App() {
   return <div>Check the console</div>
