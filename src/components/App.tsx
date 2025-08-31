56275114a81f2ee4926936a54b3346e99657c24c
Vlad-dualv <147710428+Vlad-dualv@users.noreply.github.com>
2025-08-31 14:12:08 +0100

    exercise 3

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,3 @@
-type Role = "guest" | "admin"
-
-interface User {
-  name: string;
-  age?: number;
-  email: string;
-  role: Role;
-}
-
-function checkAdmin(user: User): boolean {
-  if (user.role === "admin") {
-    return true
-  } else {
-    return false
-  }
-}
-
-console.log(checkAdmin({
-  name: "John",
-  age: 30,
-  email: "example@example.com",
-  role: "guest"
-}))
 
 
 export default function App() {
