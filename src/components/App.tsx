8ed91a74b463bfa8c5d09c30abc8364acd4615b6
Vlad-dualv <147710428+Vlad-dualv@users.noreply.github.com>
2025-09-02 12:29:39 +0100

    exercise 3

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,21 @@
-interface User {
-  id: string;
-  name: string;
-  age: number;
-  phoneNumber?: number;
+interface ObjectResult {
+  sum: number;
+  average: number;
+  count: number;
 }
 
-function greeting(user: User): string {
-  return ` Hello, ${user.name}! Your id is ${user.id} and your age is ${user.age}`
+function calculateNumbers(numbers: number[]): ObjectResult {
+  const sum = numbers.reduce((acc, i) => acc + i, 0)
+  const count = numbers.length
+  const average = count === 0 ? 0 : sum /count
+  return {
+    sum,
+    average,
+    count
+  }
 }
 
-const user1: User = {
-  id: crypto.randomUUID(),
-  name: "Vlad",
-  age: 30,
-}
-
-console.log(greeting(user1))
+console.log(calculateNumbers([1, 2, 3, 4]))
 
 export default function App() {
   return <div>Check the console for results</div>
