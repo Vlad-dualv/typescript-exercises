c180e20612848ac9a9262d7476703fab1963d719
Vlad-dualv <147710428+Vlad-dualv@users.noreply.github.com>
2025-09-02 12:36:58 +0100

    exercise 4

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,12 @@
-interface ObjectResult {
-  sum: number;
-  average: number;
-  count: number;
-}
-
-function calculateNumbers(numbers: number[]): ObjectResult {
-  const sum = numbers.reduce((acc, i) => acc + i, 0)
-  const count = numbers.length
-  const average = count === 0 ? 0 : sum /count
-  return {
-    sum,
-    average,
-    count
+function calculate(input: string | number): number {
+  if (typeof input === "string") {
+    return input.length
+  } else {
+ return input * input
   }
+   
+  
 }
-
-console.log(calculateNumbers([1, 2, 3, 4]))
 
 export default function App() {
   return <div>Check the console for results</div>
