eea3d607e25134593c8b9e65e9e5c80c1e0d9465
Vlad-dualv <147710428+Vlad-dualv@users.noreply.github.com>
2025-09-03 16:49:49 +0100

    exercise 8

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,41 +1,18 @@
-class BankAccount {
-  constructor(public accountNumber: number, private balance: number, public owner: string) {}
-
-  checkBalance(): number {
-    return this.balance;
+function format(input: Date | number, decimalPlaces?: number): string {
+  if (input instanceof Date) {
+    const yearToString = String(input.getFullYear())
+    const monthToString = String(input.getMonth() + 1).padStart(2, "0")
+    const dayToString = String(input.getDate()).padStart(2, "0")
+    return `${monthToString}/${dayToString}/${yearToString}`
+  } else if (typeof input === "number" && decimalPlaces !== undefined) {
+    return String(input.toFixed(decimalPlaces))
   }
-
-  deposit(amount: number): boolean {
-    if (amount > 0) {
-      this.balance += amount;
-      console.log(`${amount} added to account`)
-      return true
-    } else {
-      console.log(`The amount of deposit must be more that 0`)
-    }
-  }
-
-  withdraw(amount: number): boolean {
-    if (amount > 0 && this.balance >= amount) {
-      this.balance -= amount
-      console.log(`${amount} withdrawn from account`)
-      return false
-    } else if (amount <= 0) {
-      console.log(`The amount of withdraw must be more than 0`)
-      return true
-    } else {
-      console.log("No sufficient account balance")
-      return false
-    }
-  }
+  return "Invalid input"
 }
 
-const account1 = new BankAccount(12345, 1000, "John")
-console.log(account1.checkBalance())
-account1.deposit(500)
-account1.withdraw(200)
-account1.checkBalance()
-console.log(account1.checkBalance())
+console.log(format(2.12356, 2))
+const myDate = new Date(2023, 11, 25); // December 25, 2023 (remember month is 0-indexed)
+console.log(format(myDate)); 
 
 export default function App() {
   return <div>Check the console for results</div>
