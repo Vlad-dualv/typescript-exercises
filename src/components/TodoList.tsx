539b9df9e4e2812b372e24552e7971126a9d9b87
Vlad-dualv <147710428+Vlad-dualv@users.noreply.github.com>
2025-09-07 11:46:20 +0100

    Todo exercise func

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -32,12 +32,44 @@ export default function TodoList() {
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault()
-        
+        const trimmedInput = inputValue.trim()
+        if (trimmedInput === "") {
+            alert("Please enter a todo!")
+            return
+        }
+
+        if(trimmedInput.length > 50) {
+            alert("Todo is too long!")
+            return
+        }
+
+        const isDuplicate = todos.some(todo => todo.text.toLowerCase() === inputValue.trim().toLocaleLowerCase())
+
+        if (isDuplicate) {
+            alert("This todo already exists!")
+            return
+        }
+
+        addTodo(trimmedInput)
+        setInputValue("")
     }
 
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+                placeholder="Add todo..."/>
+                <button type="submit">Add Todo</button>
+            </form>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        <p style={{textDecoration: todo.completed ? "line-through" : "none"}}>{todo.text}</p>
+                        <button type="button" onClick={() => toggleTodo(todo.id)}>{todo.completed ? "Incomplete" : "Complete"}</button>
+                        <button type="button" onClick={() => deleteTodo(todo.id)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
